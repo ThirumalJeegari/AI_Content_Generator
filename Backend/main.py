eb50c7dd6af9b21299dfc71901f1a9550d99f370
ThirumalJeegari <thirumaljeegari21@gmail.com>
2026-06-01 16:28:12 +0530

    Initial Commit

---
+++ Backend/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from groq import Groq
+import os
+
+app = FastAPI()
+
+client =Groq(api_key = os.getenv("api_key"))
+
+
+
+
+@app.get("/")
+def home():
+    return {"message": "Backend Running Successfully"}
+
+
+
+@app.Post("/generate")
+def generate(topic:str,technology:str,content:str,tone:str):
+
+    prompt = f"""
+        Generate a {content} on Topic name : {topic} with technology is{technology} and tone as {tone}
+
+    """
+
+    response = client.chat.completions.create(
+        model = "llama-3.1-8b-instant",
+        message = [
+            {
+                "role" : "user",
+                "content":prompt
+
+            }
+        ]
+    )
+
+    return{
+        "response": response.choices[0].message.content
+    }
---
+++ Frontend/app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import requests
+
+server_url = st.secrets["backend_url"]
+
+
+st.title("AI Content Generator")
+
+with st.form("Generate Blogs, LinkedIn Posts, Captions, Emails and many more"):
+    topic = st.text_input("Enter the Topic to Generate the Context")
+
+    technology = st.selectbox("Enter the Technology",["Python","MySQL","Power BI","React Js","Node Js","Fast API","AI","Gen AI","MERN"])
+
+    content =  st.selectbox("Content Type",["LinkedIn Post","Blog","Instagram Caption","Twitter Post","Email","YouTube Description"]) 
+
+    tone = st.selectbox("Enter the tone",["Professional","Semi-Professional","Technical","Friendly","Casual","Marketing"])
+
+    Submit_Button = st.form_submit_button("Submit")
+
+    if Submit_Button:
+
+        if topic == "":
+            st.error("Please Enter the Topic Name")
+
+        else:
+            st.spinner("Generating Content...")
+
+
+            response = requests.post(f"{server_url}/generate",details ={"topic":topic,"technology":technology,"content":content,"tone":tone})
+
+            if response.status_code == 200:
+                data = response.json()
+
+            else:
+                st.error(data)
+
+
+   
